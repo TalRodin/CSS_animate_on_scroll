af49dcd7e04edef7b0474c7829c7665f15d51f2b
Alyona <alyona.rodin@gmail.com>
2019-10-14 03:18:10 -0400

    first commit

---
+++ src/first_css.js
@@ -0,0 +1,25 @@
+import { TimelineMax } from "gsap";
+import ScrollMagic from 'scrollmagic';
+import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap'
+
+
+console.log('Hello')
+// var t1 = new TimelineMax({onUpdate:updatePercentage});
+// const controller = new ScrollMagic.Controller();
+
+// t1.from('blockquote', 0.5, {x:200,opacity:0})
+
+// const scene = new ScrollMagic.Scene({
+//     triggerElement:".sticky",
+//     triggerHook: "onLeave",
+//     duration: "100%"
+// })
+//     .setPin(".sticky")
+//     .setTween(t1)
+//         .addTo(controller);
+
+// function updatePercentage(){
+//     t1.progress();
+//     console.log(t1.progress())
+}
+
